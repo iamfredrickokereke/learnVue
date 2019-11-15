9223ed3f124071a65a14774dcd20141730b355c3
Kelly online <kellyonline2016@gmail.com>
2019-11-15 23:45:56 +0100

    modify text heading

--- VueCli/my-vue-app-project/src/App.vue
+++ VueCli/my-vue-app-project/src/App.vue
@@ -26,7 +26,7 @@ export default {
   },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Welcome to My Vue.js App',
       friends:[
                      
                     {name : 'fred', status : true},
